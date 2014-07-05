9056ded74bfd54edc2619a5831ad9c8c0932c25d
Lloyd Davies <lloyd.davies@gamesys.co.uk>
2014-07-05 21:32:48 +0100

    Tidying up

--- email_scraper.py
+++ email_scraper.py
@@ -4,19 +4,15 @@ import sys
 import urllib
 import re
 
-EMAIL_REGEX = r'[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}'
+email_regex = r'[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}'
 
 def main():
     url = sys.argv[1]
     content = urllib.urlopen(url)
     info = content.info()
-
     assert info.gettype() == 'text/html', 'Content not of type text/html'
-
     text = content.read()
-
     emails = set(re.findall(EMAIL_REGEX, text, re.IGNORECASE))
-
     for email in emails:
         print email
 
